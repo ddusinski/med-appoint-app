749497ef6fed0f98db4919241b6e363bf7b4e86c
dominik.dusinski <dominik.dusinski@gmail.com>
2023-09-25 16:07:18 +0200

    Change routing to WebAdapterConfig

---
+++ src/main/java/com/dusinski/config/WebAdapterConfig.java
@@ -0,0 +1,16 @@
+package com.dusinski.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebAdapterConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+        registry.addViewController("/appointments").setViewName("forward:/index.html");
+        registry.addViewController("/addappointment").setViewName("forward:/index.html");
+    }
+}
